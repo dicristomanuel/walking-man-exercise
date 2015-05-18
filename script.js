f19d87b43d5cefd514f6084bac114de8b1d73668
dicristomanuel <dicristomanuel@icloud.com>
2015-05-18 10:07:08 -0700

    The man can walk

---
+++ script.js
@@ -0,0 +1,41 @@
+
+var man = document.querySelector("img");
+var marginLeft = 0
+document.body.style.borderBottom = "5px solid lightgreen";
+	
+	// man.addEventListener("click", function() {
+// 	this.style.marginLeft = marginLeft + "px";
+// 	marginLeft += 20;
+// });
+
+
+var walkRight = function() {
+	setInterval(function(){
+		man.style.transform = "rotateZ(0deg)";
+		man.style.marginLeft = marginLeft + "px";
+		if (marginLeft < 960) {
+			marginLeft += 20;
+		} else if(marginLeft >= 880 && marginLeft <= 940)  { 
+		man.style.transform = "rotateZ(270deg)";
+		console.log("here");
+			marginLeft += 20;
+		} else {
+			walkLeft();
+		}//if statement
+	}, 200); //setInterval
+}; //walk
+
+
+var walkLeft = function() {
+	setInterval(function(){
+		man.style.marginLeft = marginLeft + "px";
+		man.style.transform = "rotateZ(180deg)";
+		if (marginLeft > 0) {
+			marginLeft -= 20;
+		} else {
+			walkRight();
+		}//if statement
+	}, 200); //setInterval
+}; //walk
+
+walkRight();
